2ed9359aa0fb47f0fcf550a5a1cbd54e59eb9492
Shashi Kiran <shashi@Shashis-MacBook-Pro-2.local>
2025-11-11 20:25:41 +0530

    landing page

--- src/app/modules/landing-page/landing-page.component.ts
+++ src/app/modules/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
